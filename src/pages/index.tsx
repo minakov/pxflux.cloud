3afce71fc48d8d8f99319c830f889171e5e74f0d
Vladimir Minakov <minakov@gmail.com>
2022-03-22 15:30:13 +0000

    Fix workers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,25 @@
 import type { NextPage } from 'next'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import FileInput from '../components/FileInput'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   const fileRef = useRef<HTMLInputElement>(null)
-  const workerRef = useRef<Worker>(null);
+  const workerRef = useRef<Worker>();
   const [messages, setMessages] = useState<String[]>([]);
 
   useEffect(() => {
     workerRef.current = new Worker(new URL('../workers/upload.worker.ts', import.meta.url));
     workerRef.current.addEventListener('message', (evt) => {
       console.log('Message from TS worker:', evt.data);
-      setMessages([...messages, evt.data]);
+      setMessages(data => [...data, evt.data]);
     });
     workerRef.current.postMessage({ type: 'start' });
   }, []);
 
   const handleChange = () => {
     fileRef.current?.click();
-};
+  };
 
   return (
     <div className={styles.container}>
