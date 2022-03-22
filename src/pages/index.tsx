582bce7a80011208b726a29bf6f9792579761268
Vladimir Minakov <minakov@gmail.com>
2022-03-22 19:29:35 +0000

    Add authorization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,4 +18,23 @@ const Home: NextPage = () => {
   )
 }
 
+export async function getStaticProps() {
+  const auth =  Buffer.from(`${process.env.BACKBLAZE_KEY_ID}:${process.env.BACKBLAZE_APP_KEY}`).toString('base64')
+  try {
+    const response = await fetch('https://api.backblazeb2.com/b2api/v2/b2_authorize_account', {
+      headers: {
+        Authorization: `Basic ${auth}`
+      }
+    });
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error: any) {
+    console.log(error.message);
+  }
+  return { props: {} }
+}
+
 export default Home
